dbcceca5ab742e2c6c475cdc1e3947f109634194
Artem Melnikov <a.melnikov@carex.su>
2016-11-25 16:05:36 +0600

    New 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,33 @@ public class Main {
     public static void main(String[] args) {
         // Создание первого объекта на основе класса Rectangle
         Rectangle myRectangle1 = new Rectangle();
+        Rectangle[] Rectangles = new Rectangle[25];
+
+        for(int i=0; i<25; i++){
+            int X = -20 + (int)(Math.random() * 40);
+            int X1 = -20 + (int)(Math.random() * 40);
+            int Y = -20 + (int)(Math.random() * 40);
+            int Y1 = -20 + (int)(Math.random() * 40);
+            Rectangles[i] = new Rectangle();
+            Rectangles[i].SetFXY(X,X1,Y,Y1);
+        }
+
+        for(int i=0; i<25; i++){
+            for (int j=0; j<25; j++){
+                if(i!=j) {
+                    if (i < j) {
+                        if (check(Rectangles[i], Rectangles[j])) {
+                            System.out.println("Прямоугольник " + i + " Пересекается с прямоугольником " + j);
+                        }
+                    }
+                }
+            }
+        }
 
         // Установим значение полей
         myRectangle1.FSideA = 5;
         myRectangle1.FSideB = 10;
-        myRectangle1.SetFXY(1, 3, 1, -1);
+        myRectangle1.SetFXY(-1, -2, -1, -2);
 
 
         // Вызовим метод изменения названия фигуры
@@ -34,7 +56,7 @@ public class Main {
         // изменения названия фигуры
         myRectangle2.SetName("Маленький прямоугольник");
         myRectangle2.SetFSide(2, 5);
-        myRectangle2.SetFXY(0, 2, 2, 1);
+        myRectangle2.SetFXY(0, 3, 0, -3);
 
         // определения периметра фигуры
         int Perimeter2 = myRectangle2.GetPerimeter();
